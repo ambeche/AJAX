01951e0a61c4f42b16b7295ffeb53cd3969a361b
Tamanji Che <cheambetamanji@gmail.com>
2019-12-04 00:04:47 +0200

    AJAX implemented to fetch html content and load images

---
+++ js/main-a.js
@@ -0,0 +1,32 @@
+'use strict'
+
+const ul = document.querySelector('ul');
+
+const showImages = async (code) => {
+
+    ul.innerHTML= await  code;
+
+};
+
+const getFetchData = async (url) => {
+
+    const response = await fetch(url);
+
+    const result = await response.text();
+
+    return result;
+
+};
+
+try {
+
+const code = getFetchData('images.html');
+
+showImages(code);
+
+} catch {
+
+    const error=err;
+
+    console.log(error);
+}
